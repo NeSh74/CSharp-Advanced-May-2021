73f68f548f0026a08bae2465dbf26a3aef734451
NeSh74 <nejdie@yahoo.com>
2021-06-09 07:56:54 +0300

    add
    new

---
+++ 6. Defining Classes/Exercise_Defining_Classes/2.Creating_Constructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person();
+
+            Person personTwo = new Person();
+
+            Person presonThree = new Person();
+
+            Console.WriteLine($"{personOne.Name} {personOne.Age}");
+            Console.WriteLine($"{personTwo.Name} {personTwo.Age}");
+            Console.WriteLine($"{presonThree.Name} {presonThree.Age}");
+        }
+    }
+}
