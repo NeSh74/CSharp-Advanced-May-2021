bcbc48406e16035ed394765f4a254c01bb8815c7
NeSh74 <nejdie@yahoo.com>
2021-06-19 20:04:29 +0300

    add
    new

---
+++ CSharp Advanced Exam preparation/CSharp_Advanced_Exam_25_October_2020/01.Scheduling/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var task = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            //var threads = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            //var taskToBeKilled = int.Parse(Console.ReadLine());
+
+            //while (true)
+            //{
+            //    if (task.Peek() == taskToBeKilled)
+            //    {
+            //        break;
+            //    }
+
+            //    if (task.Peek() <= threads.Peek())
+            //    {
+            //        task.Pop();
+            //        threads.Dequeue();
+            //    }
+            //    else
+            //    {
+            //        threads.Dequeue();
+            //    }
+            //}
+
+            //Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskToBeKilled}");
+            //Console.WriteLine(string.Join(" ", threads));
+
+            var task = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            var threads = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            var taskToBeKilled = int.Parse(Console.ReadLine());
+
+            while (task.Peek() != taskToBeKilled)
+            {
+                if (task.Peek() <= threads.Peek())
+                {
+                    task.Pop();
+                    threads.Dequeue();
+                }
+                else
+                {
+                    threads.Dequeue();
+                }
+            }
+
+            Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskToBeKilled}");
+            Console.WriteLine(string.Join(" ", threads));
+
+        }
+    }
+}
---
+++ CSharp Advanced Exam preparation/CSharp_Advanced_Exam_25_October_2020/03.Classroom/Classroom.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+        public int Capacity { get; }
+
+        public int Count => students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if (Capacity <= Count)
+            {
+                return "No seats in the classroom";
+            }
+
+            students.Add(student);
+            return $"Added student {student.FirstName} {student.LastName}";
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            bool isStudentExisted = students.Exists(x => x.FirstName == firstName && x.LastName == lastName);
+            if (isStudentExisted)
+            {
+                students.Remove(students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName));
+                return $"Dismissed student {firstName} {lastName}";
+            }
+
+            return $"Student not found";
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            var studentSubject = students.Where(x => x.Subject == subject).ToList();
+            if (studentSubject.Count > 0)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+                foreach (Student student in studentSubject)
+                {
+                    sb.AppendLine($"{student.FirstName} {student.LastName}");
+                }
+
+                return sb.ToString().TrimEnd();
+            }
+
+            return $"No students enrolled for the subject";
+        }
+
+        public int GetStudentsCount() => Count;
+
+        //public Student GetStudent(string firstName, string lastName)
+        //{
+        //    var currentStudent = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+        //    return currentStudent;
+        //}
+
+        public Student GetStudent(string firstName, string lastName) =>
+           students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+    }
+}
