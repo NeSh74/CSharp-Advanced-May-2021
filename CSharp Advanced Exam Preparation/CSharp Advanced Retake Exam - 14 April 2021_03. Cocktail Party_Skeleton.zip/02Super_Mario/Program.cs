c4f6b6bc3b11655dea8e3a0d637e7ebf3b5eab1b
NeSh74 <nejdie@yahoo.com>
2021-06-19 21:49:28 +0300

    add
    new

---
+++ CSharp Advanced Exam Preparation/CSharp Advanced Retake Exam - 14 April 2021_03. Cocktail Party_Skeleton.zip/02Super_Mario/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace _02Super_Mario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lives = int.Parse(Console.ReadLine());
+            var rows = int.Parse(Console.ReadLine());
+
+            char[][] maze = new char[rows][];
+            var currRow = 0;
+            var currCol = 0;
+
+            for (int row = 0; row < maze.GetLength(0); row++)
+            {
+                var mazeRow = Console.ReadLine();
+                maze[row] = new char[mazeRow.Length];
+
+                for (int col = 0; col < mazeRow.Length; col++)
+                {
+                    maze[row][col] = mazeRow[col];
+                    if (maze[row][col] == 'M')
+                    {
+                        currRow = row;
+                        currCol = col;
+                    }
+                }
+            }
+
+            bool missionAcomplished = false;
+            while (true)
+            {
+                var moves = Console.ReadLine().Split();
+                var direction = char.Parse(moves[0]);
+                var spawnRow = int.Parse(moves[1]);
+                var spawnCol = int.Parse(moves[2]);
+
+                maze[spawnRow][spawnCol] = 'B';
+                lives--;
+                switch (direction)
+                {
+                    case 'W':
+                        if (currRow - 1 < 0)
+                        {
+                            continue;
+                        }
+
+                        maze[currRow][currCol] = '-';
+                        currRow--;
+                        break;
+                    case 'S':
+                        if (currRow + 1 == rows)
+                        {
+                            continue;
+                        }
+
+                        maze[currRow][currCol] = '-';
+                        currRow++;
+                        break;
+                    case 'A':
+                        if (currCol - 1 < 0)
+                        {
+                            continue;
+                        }
+
+                        maze[currRow][currCol] = '-';
+                        currCol--;
+                        break;
+                    case 'D':
+                        if (currCol + 1 == maze[currRow].Length)
+                        {
+                            continue;
+                        }
+
+                        maze[currRow][currCol] = '-';
+                        currCol++;
+                        break;
+                }
+
+                if (lives <= 0)
+                {
+                    maze[currRow][currCol] = 'X';
+                    break;
+                }
+
+                if (maze[currRow][currCol] == 'B')
+                {
+                    lives -= 2;
+                    if (lives <= 0)
+                    {
+                        maze[currRow][currCol] = 'X';
+                        break;
+                    }
+                }
+                else if (maze[currRow][currCol] == 'P')
+                {
+                    missionAcomplished = true;
+                    maze[currRow][currCol] = '-';
+                    break;
+                }
+
+                maze[currRow][currCol] = 'M';
+            }
+
+            if (missionAcomplished)
+            {
+                Console.WriteLine($"Mario has successfully saved the princess! Lives left: {lives}");
+            }
+            else
+            {
+                Console.WriteLine($"Mario died at {currRow};{currCol}.");
+            }
+
+            foreach (char[] row in maze)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+    }
+}
