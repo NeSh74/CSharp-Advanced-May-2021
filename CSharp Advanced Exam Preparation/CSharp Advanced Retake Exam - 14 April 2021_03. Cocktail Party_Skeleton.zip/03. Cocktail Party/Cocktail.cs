7d2e5cc87a88a0d567fdd80290088e8ecd0779fc
NeSh74 <nejdie@yahoo.com>
2021-06-21 11:24:22 +0300

    Revert "Delete CSharp Advanced Exam Preparation directory"
    This reverts commit a3374c74db78ae7dc8b79b45acd7e6ef2506756d.

---
+++ CSharp Advanced Exam Preparation/CSharp Advanced Retake Exam - 14 April 2021_03. Cocktail Party_Skeleton.zip/03. Cocktail Party/Cocktail.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        private List<Ingredient> ingredients;
+
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            ingredients = new List<Ingredient>();
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int MaxAlcoholLevel { get; set; }
+
+        public int CurrentAlcoholLevel => ingredients.Sum(alcohol => alcohol.Alcohol);
+
+
+        public void Add(Ingredient ingredient)
+        {
+            bool isIngredient = false;
+            foreach (Ingredient element in ingredients)
+            {
+                if (element.Name == ingredient.Name)
+                {
+                    isIngredient = true;
+                }
+            }
+
+            if (!isIngredient)
+            {
+                if (ingredients.Count < Capacity && CurrentAlcoholLevel <= MaxAlcoholLevel)
+                {
+                    ingredients.Add(ingredient);
+                }
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            bool isIngredient = false;
+            foreach (Ingredient ingredient in ingredients)
+            {
+                if (ingredient .Name ==name)
+                {
+                    isIngredient = true;
+                }
+            }
+
+            if (isIngredient )
+            {
+                ingredients.Remove(ingredients.FirstOrDefault(ingredient => ingredient.Name == name));
+                return true;
+            }
+
+            return false;
+        }
+
+        //public Ingredient FindIngredient(string name) => ingredients.FirstOrDefault(n => n.Name == name);
+        public Ingredient FindIngredient(string name)
+        {
+            return ingredients.FirstOrDefault(n => n.Name == name);
+            //foreach (Ingredient ingredient in ingredients)
+            //{
+            //    if (ingredient .Name ==name)
+            //    {
+            //        return ingredient;
+            //    }
+
+            //    return null;
+            //}
+        }
+
+        public Ingredient GetMostAlcoholicIngredient() =>
+            ingredients.OrderByDescending(i => i.Alcohol).FirstOrDefault();
+
+        public string Report()
+        {
+            var result = $"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}\r\n";
+            foreach (Ingredient ingredient in ingredients)
+            {
+                result += $"{ingredient.ToString()}\r\n";
+            }
+
+            return result.TrimEnd();
+        }
+    }
+}
---
+++ CSharp Advanced Exam Preparation/CSharp_Advanced_Exam_20_February_2021/02.Warships/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Warships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            string[] input = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            int playerOneShips = 0;
+            int playerTwoShips = 0;
+            int totalCountShipsDestroyed = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] rowData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+
+                    if (matrix[row, col] == '<')
+                    {
+                        playerOneShips++;
+                    }
+                    else if (matrix[row, col] == '>')
+                    {
+                        playerTwoShips++;
+                    }
+                }
+            }
+
+            bool isSomeoneWon = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int[] currentCoordinate = input[i]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int currRow = currentCoordinate[0];
+                int currCol = currentCoordinate[1];
+
+                if (currRow < 0 || currRow >= n || currCol < 0 || currCol >= n)
+                {
+                    continue;
+                }
+
+                if (IsPlayerOneShip(matrix, currRow, currCol))
+                {
+                    playerOneShips--;
+                    matrix[currRow, currCol] = 'X';
+                    totalCountShipsDestroyed++;
+                }
+                else if (IsPlayerTwoShip(matrix, currRow, currCol))
+                {
+                    playerTwoShips--;
+                    matrix[currRow, currCol] = 'X';
+                    totalCountShipsDestroyed++;
+                }
+                else if (matrix[currRow, currCol] == '#')
+                {
+                    for (int row = currRow - 1; row <= currRow + 1; row++)
+                    {
+                        for (int col = currCol - 1; col <= currCol + 1; col++)
+                        {
+                            if (row >= 0 && row < n &&
+                                col >= 0 && col < n)
+                            {
+                                if (IsPlayerOneShip(matrix, row, col))
+                                {
+                                    playerOneShips--;
+                                    matrix[row, col] = 'X';
+                                    totalCountShipsDestroyed++;
+                                }
+                                else if (IsPlayerTwoShip(matrix, row, col))
+                                {
+                                    playerTwoShips--;
+                                    matrix[row, col] = 'X';
+                                    totalCountShipsDestroyed++;
+                                }
+                            }
+                        }
+
+                        if (playerOneShips == 0)
+                        {
+                            isSomeoneWon = true;
+                            break;
+                        }
+                        else if (playerTwoShips == 0)
+                        {
+                            isSomeoneWon = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (playerOneShips <= 0)
+                {
+                    Console.WriteLine($"Player Two has won the game! {totalCountShipsDestroyed} ships have been sunk in the battle.");
+                    break;
+                }
+                else if (playerTwoShips <= 0)
+                {
+                    Console.WriteLine($"Player One has won the game! {totalCountShipsDestroyed} ships have been sunk in the battle.");
+                    break;
+                }
+            }
+
+            if (playerOneShips > 0 && playerTwoShips > 0)
+            {
+                Console.WriteLine($"It's a draw! Player One has {playerOneShips} ships left. Player Two has {playerTwoShips} ships left.");
+            }
+        }
+
+        public static bool IsPlayerOneShip(char[,] field, int row, int col)
+        {
+            return field[row, col] == '<';
+        }
+
+        public static bool IsPlayerTwoShip(char[,] field, int row, int col)
+        {
+            return field[row, col] == '>';
+        }
+    }
+}
