c88ae39f45ed66bb93bb8beae3d0862cfc5da37a
NeSh74 <nejdie@yahoo.com>
2021-06-26 13:41:46 +0300

    add
    new

---
+++ Exam_Csharp_Advance_26_06_2021/02.Survivor/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Survivor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                matrix[row] = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+            }
+
+            int collectedTokens = 0;
+            int opponentTokens = 0;
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Gong")
+            {
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+
+                switch (command)
+                {
+                    case "Find":
+
+                        if (IsValid(matrix, row, col))
+                        {
+                            if (matrix[row][col] == 'T')
+                            {
+                                matrix[row][col] = '-';
+
+                                collectedTokens++;
+                            }
+                        }
+
+                        break;
+
+                    case "Opponent":
+
+                        string direction = tokens[3];
+
+                        if (IsValid(matrix, row, col))
+                        {
+                            if (matrix[row][col] == 'T')
+                            {
+                                matrix[row][col] = '-';
+
+                                opponentTokens++;
+                            }
+
+                            for (int i = 0; i < 3; i++)
+                            {
+                                switch (direction)
+                                {
+                                    case "up": row--; break;
+                                    case "down": row++; break;
+                                    case "left": col--; break;
+                                    case "right": col++; break;
+                                }
+
+                                if (IsValid(matrix, row, col))
+                                {
+                                    if (matrix[row][col] == 'T')
+                                    {
+                                        matrix[row][col] = '-';
+
+                                        opponentTokens++;
+                                    }
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Collected tokens: {collectedTokens}");
+            Console.WriteLine($"Opponent's tokens: {opponentTokens}");
+        }
+
+        private static bool IsValid(char[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) &&
+                   col >= 0 && col < matrix[row].Length;
+        }
+    }
+}
---
+++ Exam_Csharp_Advance_26_06_2021/03.SkiRental/SkiRental.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        private List<Ski> data;
+
+        public SkiRental()
+        {
+            this.data = new List<Ski>();
+        }
+
+        public SkiRental(string name, int capacity)
+            : this()
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+
+
+        public void Add(Ski ski)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(ski);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            Ski skiToremove = this.data.FirstOrDefault(m => m.Manufacturer == manufacturer && m.Model == model);
+
+            if (skiToremove == null)
+            {
+                return false;
+            }
+
+            this.data.Remove(skiToremove);
+            return true;
+        }
+
+        public Ski GetNewestSki()
+        {
+            return this.data
+                .OrderByDescending(a => a.Year)
+                .FirstOrDefault();
+        }
+
+        public Ski GetSki(string manufacturer, string model)
+        {
+            Ski ski = this.data
+                .FirstOrDefault(m => m.Manufacturer == manufacturer && m.Model == model);
+            return ski;
+        }
+        public int Count => this.data.Count;
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The skis stored in {this.Name}:");
+            foreach (Ski ski in this.data)
+            {
+                sb.AppendLine(ski.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
